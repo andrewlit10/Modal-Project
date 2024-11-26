2d91dd1b2ae060ec418e8701ceb46830bebd0f80
Andrew Litten <andrew.litten@gmail.com>
2024-11-26 11:54:20 -0800

    added JavaScript functionality
    added event listeners to open and close the modal

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // select modal-btn,modal-overlay,close-btn
+const modalBtn = document.querySelector('.modal-btn')
+const modelOverlay = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn')
 // listen for click events on modal-btn and close-btn
+modalBtn.addEventListener('click', function(){
+    modelOverlay.classList.toggle('open-modal')
+})
+closeBtn.addEventListener('click', function(){
+    modelOverlay.classList.toggle('open-modal')
+})
+
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
